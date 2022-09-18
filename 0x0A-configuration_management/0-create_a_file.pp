e61e56513af22ddf6e50a229f01c50ad7888b5f5
hussein alamutu <alamutuhussein@gmail.com>
2022-09-18 09:36:09 +0100

    created readme file and a puppet code to automate the creation of a file with content(I love Puppet).

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in /tmp with the content 'I love Puppet' using puppet cm tool
+file { '/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => 0744,
+  content  => 'I love Puppet'
+  }
